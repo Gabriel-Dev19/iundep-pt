e6fe97ba0545abcccc54a1cd91b852a5cf67d3a8
DESKTOP-NOHUJUV\Rosiane Queiroz <dyeybson@gmail.com>
2022-08-23 08:40:04 -0300

    atualização de texto.

--- components/Home.vue
+++ components/Home.vue
@@ -236,7 +236,7 @@
             <div class="container mt-xs-70 mt-sm-50 mt-xl-120 d-flex align-items-center row mx-auto justify-content-xs-center justify-content-sm-center justify-content-xl-between position-absolute">
               <div class="col-md-10 col-xl-5 animate__animated animate__fadeInLeft px-0">
                 <h1 class="text-white text-xs-center text-sm-center text-xl-left fs-sm-45">
-                  <span class="text-azul-light fw-600">Localizamos pessoas</span> em qualquer parte do <span class="text-azul-light fw-600">Portugal</span>
+                  <span class="text-azul-light fw-600">Localizamos pessoas</span> em qualquer parte de <span class="text-azul-light fw-600">Portugal</span>
                 </h1>
                 <p class="text-white text-xs-center text-sm-center text-xl-left mt-3 mb-0">
                   Com nosso serviço de <span class="text-azul-light fw-700">Localização de pessoas</span> conseguimos encontrar
